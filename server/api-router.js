1403675d7a75d563a7b2cd14e8ebe8eac716932d
daffron <daffron92@gmail.com>
2017-06-15 09:17:05 +1200

    example database query/end point

--- server/api-router.js
+++ server/api-router.js
@@ -10,7 +10,20 @@ let db
 MongoClient.connect('mongodb://localhost:27017', (err, database) => {
   if (err) return console.log(err)
   db = database.db('admin') // To be changed before deployment to a database for production
+  console.log('connected to:', db.s.databaseName)
 
-// Everything inside here
+  router.get('/getbookings', (req, res) => {
+    db.authenticate(process.env.DB_USER, process.env.DB_PW, (err, result) => {
+      if (err) return console.log (err)
+      db.collection('bookings').find().toArray((err, results) => {
+        if (err) return console.log(err)
+        const testData = {
+          id: 1,
+          date: new Date().toLocaleDateString('en-GB')
+        }
+        res.json({testData})
+      })
+    })
+  })
 })
 module.exports = router
