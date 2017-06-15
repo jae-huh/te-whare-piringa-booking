12dcb22e7b02298f0df57f4d48625391f8494afa
Luke D <luke.warmwater6@gmail.com>
2017-06-15 13:19:34 +1200

    basic authentication working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import {Route, BrowserRouter} from 'react-router-dom'
+import Login from './Login'
+import Callback from './Callback'
+import Auth from '../auth'
+import history from '../auth/history'
+
+const auth = new Auth()
+
+const handleAuthentication = (nextState, replace) => {
+  if (/access_token|id_token|error/.test(nextState.location.hash)) {
+    auth.handleAuthentication()
+  }
+}
 
 const App = () => (
-  <div className='app'>
-  <h1>Hello World</h1>
-  </div>
-)
+  <BrowserRouter history={history} component={App}>
+    <div>
+      <Route path="/" render={() => <Login auth={auth} />} />
+      <Route path="/callback" render={props => {
+        console.log('hi')
+        handleAuthentication(props)
+        return <Callback {...props} />
+      }}/>
+    </div>
+  </BrowserRouter>
+  )
 
 export default App
--- server/server.js
+++ server/server.js
@@ -3,8 +3,13 @@ const router = require('./api-router')
 const path = require('path')
 const bodyParser = require('body-parser')
 const app = express()
+
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, '../public')))
 app.use('/api/v1/', router)
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+})
 
 module.exports = app
