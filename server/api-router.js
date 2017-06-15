8e79c9ed19358587a0842b9c68e931aa9e6812e8
daffron <daffron92@gmail.com>
2017-06-16 10:11:18 +1200

    update

--- server/api-router.js
+++ server/api-router.js
@@ -20,10 +20,9 @@ const bodyParser = require('body-parser')
 const db = require('./db')
 const router = express.Router()
 const nodemailer = require('nodemailer')
-
+const jsonwt = require('jsonwebtoken')
 const jwt = require('express-jwt')
 const jwksRsa = require('jwks-rsa')
-require('dotenv').config()
 
 router.use(bodyParser.json())
 
@@ -53,6 +52,13 @@ router.get('/getbookings', (req, res) => {
   })
 })
 
+router.get('/testing', (req, res) => {
+  console.log(req.headers.token)
+  const decoded = jsonwt.decode(req.headers.token, {complete: true})
+  console.log(decoded)
+  res.send(decoded.payload.sub)
+})
+
 // router.use(checkJwt)
 
 router.get('/admin/getbookings', (req, res) => {
@@ -70,7 +76,9 @@ router.get('/admin/getunconfirmed', (req, res) => {
     })
   })
 })
+router.get('/user/checkuser/:id', (req, res) => {
 
+})
 router.post('/user/addbooking', (req, res) => {
   db.userAddBooking(req, res, (err, result) => {
     if (err) return res.json({error: err})
@@ -86,6 +94,8 @@ router.put('/admin/confirm/:id', (req, res) => {
 })
 
 router.post('/user/adduser', (req, res) => {
+  const decoded = jwt.decode(req.body.token, {complete: true})
+  console.log(decoded)
   db.addUser(req, res, (err, result) => {
     if (err) return res.json({error: err})
     res.json(result)
