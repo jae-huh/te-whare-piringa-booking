23cf846b06f10adadcad2686fca542b7cc4f3524
daffron <daffron92@gmail.com>
2017-06-15 14:23:25 +1200

    added basic mailer and test setup

--- server/server.js
+++ server/server.js
@@ -7,4 +7,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, '../public')))
 app.use('/api/v1/', router)
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+})
 module.exports = app
