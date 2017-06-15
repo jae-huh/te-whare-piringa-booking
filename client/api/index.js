e6ac5b5c97ec35bd40dbd8c6d008e9596c95b785
daffron <daffron92@gmail.com>
2017-06-15 18:55:50 +1200

    fix an oops

--- client/api/index.js
+++ client/api/index.js
@@ -1,1 +1,9 @@
+import request from 'superagent'
 
+export function getUnconfirmed (cb) {
+  request.get('/api/v1/admin/getunconfirmed')
+  .end((err, data) => {
+    if (err) return cb(err)
+    cb(null, data.body)
+  })
+}
