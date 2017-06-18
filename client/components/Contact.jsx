a8486aad1738419a8fe6f45fd6efceccefcaea35
Jae Huh <jaeeunhuh@gmail.com>
2017-06-18 15:25:41 +1200

    Position temporarily

--- client/components/Contact.jsx
+++ client/components/Contact.jsx
@@ -5,7 +5,13 @@ const Contact = () => {
     <section className='contact-container section-container' id='contact'>
       <h1 className='home-headings'>Contact Us</h1>
       <div className='contact'>
-
+        <div id="map"></div>
+        <div className="contact-information">
+          <h2>Address</h2>
+          <p>29 Fenchurch St, Glen Innes, Auckland 1072</p>
+          <h2>Phone</h2>
+          <p>09-528 3411</p>
+        </div>
       </div>
     </section>
   )
