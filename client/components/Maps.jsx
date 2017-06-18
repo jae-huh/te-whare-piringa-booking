b44700d28c22027ca7d1cb849ae0bc54ee2dd83a
Jae Huh <jaeeunhuh@gmail.com>
2017-06-18 19:01:15 +1200

    Add google maps

--- client/components/Maps.jsx
+++ client/components/Maps.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import {Gmaps, Marker} from 'react-gmaps'
 
 const coords = {lat: -36.875865, lng: 174.869912}
-
 const params = {v: '3.exp', key: 'AIzaSyCQ9Ni14O9xt1KAJ6d4H6M_eRWIyL2liCY'}
 
 class Maps extends React.Component {
