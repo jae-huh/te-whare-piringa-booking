0e10193cbff3eec22906ba198b0bd92cc41f542e
daffron <daffron92@gmail.com>
2017-06-19 20:29:38 +1200

    try and try again

--- client/components/Error.jsx
+++ client/components/Error.jsx
@@ -3,9 +3,13 @@ import {connect} from 'react-redux'
 import {ModalContainer, ModalDialog} from 'react-modal-dialog'
 
 function Error (props) {
+  function handleClose () {
+    props.history.push('/')
+  }
+
   return (
-    <ModalContainer >
-        <ModalDialog >
+    <ModalContainer onClose={handleClose}>
+        <ModalDialog onClose={handleClose}>
           <h1>{props.errorMessage}</h1>
         </ModalDialog>
     </ModalContainer>
