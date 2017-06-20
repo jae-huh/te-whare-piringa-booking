392ceacab2c60d6be1a498f16897b9c0fb2df375
paul-brabet <pbrabet@gmail.com>
2017-06-21 11:15:19 +1200

    incomplete styling - style to take another direction

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -22,33 +22,51 @@ class Profile extends React.Component {
   showUserBookings () {
     return this.props.bookings.filter(booking => booking.authId).map((booking, i) => {
       return (
-        <tr key={i}>
-          <td>{moment(booking.startDate).format('YYYY-MM-DD HH:mm')}</td>
-          <td>{moment(booking.endDate).format('YYYY-MM-DD HH:mm')}</td>
-          <td>{booking.confirmed ? 'Confirmed' : 'Waiting to be confirmed'}</td>
-          <td>{booking.deleteRequested ? 'Delete Requested' : 'No'}</td>
-          <td><button onClick={() => this.saveBookingToStore(booking)}>View</button></td>
-          <td>{!booking.deleteRequested && <button onClick={() => this.requestBookingToBeDeleted(booking._id)}>Request Delete</button>}</td>
+        <tr key={i} className="showUserBookingRow">
+          <td>
+            {moment(booking.startDate).format('ddd DD MMM YYYY')}
+            <hr />
+            {moment(booking.startDate).format('HH:mm a')} to {moment(booking.endDate).format('HH:mm a')}
+            <hr />
+            {booking.confirmed ? 'Confirmed' : 'Waiting for confirmation from admin'}
+            <hr />
+            {booking.deleteRequested ? 'Delete Requested' : 'You have not requested the event to be deleted'}
+          </td>
+          <td>
+            <button onClick={() => this.saveBookingToStore(booking)}>View</button>
+            {!booking.deleteRequested && <button onClick={() => this.requestBookingToBeDeleted(booking._id)}>Request Delete</button>}
+          </td>
         </tr>
       )
     })
   }
 
+  // <td>{moment(booking.startDate).format('HH:mm ddd DD MMM YYYY')}</td>
+  // <td>{moment(booking.endDate).format('HH:mm ddd DD MMM YYYY')}</td>
+  // <td>{booking.confirmed ? 'Confirmed' : 'Awaiting confirmation'}</td>
+  // <td>{booking.deleteRequested ? 'Delete Requested' : 'No'}</td>
+  // <td><button onClick={() => this.saveBookingToStore(booking)}>View</button></td>
+  // <td>{!booking.deleteRequested && <button onClick={() => this.requestBookingToBeDeleted(booking._id)}>Request Delete</button>}</td>
+
+  // {item.fullName}<hr />
+  // {item.startDate.toString().substring(0, 16)} to {item.endDate.toString().substring(0, 16)}<hr />
+  // {item.startDate.toString().substring(16, 21)} to {item.endDate.toString().substring(16, 21)}
+
   render () {
     return (
       <div className='profile-container'>
-        <h1 className='profile-title'>Profile</h1>
-        <h2>Your Bookings</h2>
         <div className='profile'>
+          <h2 className='profile-title'>Welcome, {this.props.user.fullName}</h2>
+          <h3>Your Bookings</h3>
           <table className='profile-table'>
-            <thead>
+            {/* <thead>
               <tr>
                 <th>Start Time</th>
                 <th>End Time</th>
                 <th>Confirmation Status</th>
                 <th>Delete Requested</th>
               </tr>
-            </thead>
+            </thead> */}
             <tbody>
               {this.showUserBookings()}
             </tbody>
@@ -71,7 +89,8 @@ function mapStateToProps (state) {
   return {
     booking: state.booking,
     bookings: state.bookings,
-    authId: state.user.authId
+    authId: state.user.authId,
+    user: state.user
   }
 }
 
