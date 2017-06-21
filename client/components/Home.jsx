fe60a3bf16b40388d8a9457f83c234899e86e7aa
Luke D <luke.warmwater6@gmail.com>
2017-06-21 17:55:55 +1200

    removed hashlink from wrong file

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {HashLink as Link} from 'react-router-hash-link'
+import {Link} from 'react-router-dom'
 
 import Hall from './Hall'
 import Gallery from './Gallery'
