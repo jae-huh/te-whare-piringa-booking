1fb7c87079ae60854ddbdddd8e95c0473c41f464
Don Smith <don@antiantonym.com>
2019-06-10 14:48:53 +1200

    get tests passing

--- test/client/components.test.js
+++ test/client/components.test.js
@@ -1,10 +1,9 @@
-import test from 'ava'
 import React from 'react'
 import {shallow} from 'enzyme'
 
 import Home from '../../client/components/Home'
 
-test('has a .home-background class name', t => {
+test('has a .home-background class name', () => {
   const wrapper = shallow(<Home />)
-  t.true(wrapper.hasClass('home-background'))
+  expect(wrapper.hasClass('home-background')).toBeTruthy()
 })
